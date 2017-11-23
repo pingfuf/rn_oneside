949c75575d70b84a006f8d838123012a68a67f88
fupingfu <fupingfu@51talk.com>
2017-11-23 14:05:57 +0800

    添加

--- index.ios.js
+++ index.ios.js
@@ -3,51 +3,40 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry
 } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
 
-export default class RNOneside extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+import SplashPage from "./src/SplashPage";
+
+export default class RNOneside extends React.Component {
+
+    render() {
+        let defaultName = "First";
+        let defaultComponent = SplashPage;
+        return (
+            <Navigator
+                initialRoute={{
+                    name:defaultName,
+                    component: defaultComponent,
+                    params:{
+                        scheme: this.props.scheme,
+                        p: this.props.p
+                    }
+                }}
+                configureScene={(route) => {
+                    return Navigator.SceneConfigs.FloatFromRight;
+                }}
+                renderScene={(route, navigator) => {
+                    let Component = route.component;
+
+                    return <Component {...route.params} navigator={navigator}/>
+                }}
+            />
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('RNOneside', () => RNOneside);
